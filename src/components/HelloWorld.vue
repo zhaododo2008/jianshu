2cc4cbe5bb543049c6a0473649d4bc555363959b
zhaododo2007 <zhaododo@163.com>
2019-02-22 00:01:16 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <section id="layout-default">
+      <div class="header-wrap">
+        <header id="jianshu-header" class="header-normal">
+          <div class="logo-wrap">
+            <img src="../../static/img/nav-logo.faf216af.png" alt="logo" class="logo">
+            <span class="slogan">创作你的创作</span>
+          </div>
+          <div class="download">
+            免费下载<i class="iconfont ic-link"></i>
+          </div>
+        </header>
+        <div class="header-shim"></div>
+      </div>
+
+
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped src="../../static/css/main.css">
+</style>
